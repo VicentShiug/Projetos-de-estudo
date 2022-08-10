eeeda3048e9c76a75c6d68eb213628304ae862aa
VicentShiug <guicuras62@gmail.com>
2022-08-10 14:41:14 -0300

    adicionando a aba de usuarios configurada

--- Assistidos/list/views.py
+++ Assistidos/list/views.py
@@ -1,9 +1,11 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from list.forms import ListaForm
 from .models import Lista
+import datetime
 
 @login_required
 def lista(request):
@@ -68,10 +70,10 @@ def delete(request, id):
     lista.delete()
     return redirect('lista')
 
-@login_required 
-def user(request):
-    listassss = Lista.objects.filter(user=request.user)
-    return render(request, 'list/user.html', {'listassss' : listassss })
+# @login_required 
+# def user(request):
+#     listassss = get_object_or_404(Lista)
+#     return render(request, 'list/user.html', {'listassss' : listassss })
 
 @login_required 
 def changestatus(request, id):
@@ -92,3 +94,42 @@ def changestatus(request, id):
 
 
 
+
+
+def user(request):
+   
+    search = request.GET.get('titulo')
+    filter = request.GET.get('filter')
+    assistidorecent = Lista.objects.filter(status = 'Assistido', updated_at__gt=datetime.datetime.now()-datetime.timedelta(days=30), user=request.user).count()
+    listassistindo = Lista.objects.filter(status  = 'Assistindo', user=request.user).count()
+    listassistido = Lista.objects.filter(status = 'Assistido' ,user=request.user).count()
+    listaassistir = Lista.objects.filter(status = 'A Assistir' ,user=request.user).count()
+
+    if search:
+        listas = Lista.objects.filter(titulo__icontains=search, user=request.user)
+
+
+        listas = Lista.objects.filter(titulo__icontains=search, user=request.user)
+    
+    elif filter:
+
+        listas = Lista.objects.filter(status=filter, user=request.user)
+        
+    else:
+        listas_pg = Lista.objects.all().order_by('-created_at').filter(user=request.user)
+        paginacao = Paginator(listas_pg, 5)
+
+        page = request.GET.get('page')
+
+        listas = paginacao.get_page(page) 
+
+        # def get_queryset(self):
+            # titulo = self.objects.filter('12')
+
+
+            # return Lista.objects.filter(titulo__icontais= titulo)
+
+    return render(request, 'list/user.html', {'listas' : listas, 'assistidorecent': assistidorecent,
+                                              'listassistindo': listassistindo,
+                                              'listassistido':listassistido,
+                                              'listaassistir':listaassistir})
